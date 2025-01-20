6f7469b9232c1cb35678b3adda36856272d0e0d8
k3cman <kecman.dev@gmail.com>
2025-01-20 08:51:23 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,40 @@
+import { buttonVariants } from "@/components/ui/button";
+import { page_routes } from "@/lib/routes-config";
+import { MoveUpRightIcon, TerminalSquareIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div className="flex sm:min-h-[85.5vh] min-h-[85vh] flex-col items-center justify-center text-center px-2 sm:py-8 py-12">
+      {/* <Link
+        href="https://github.com/nisabmohd/Aria-Docs"
+        target="_blank"
+        className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4 sm:-mt-12"
+      >
+        Follow along on GitHub{" "}
+        <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
+      </Link> */}
+      <h1 className="text-3xl font-bold mb-4 sm:text-6xl">
+        This is ESL GuideBook for Frontend Developers
+      </h1>
+      <div className="flex flex-row items-center gap-5">
+        <Link
+          href={`/docs${page_routes[0].href}`}
+          className={buttonVariants({ className: "px-6", size: "lg" })}
+        >
+          Documentation
+        </Link>
+        <Link
+          href="/blog"
+          className={buttonVariants({
+            variant: "secondary",
+            className: "px-6",
+            size: "lg",
+          })}
+        >
+          Components
+        </Link>
+      </div>
+    </div>
+  );
+}
